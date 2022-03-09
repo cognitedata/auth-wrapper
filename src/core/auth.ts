9d21e5e99e6f4f35e06ab2d765488f9e676105d8
Joao Harbs <harbspj@gmail.com>
2022-03-09 15:12:59 +0100

    chore: saving progress

--- src/core/auth.ts
+++ src/core/auth.ts
@@ -1,39 +1,61 @@
-import { ICogniteAuthWrapper, ISettings, LoginMethod } from 'interfaces/common';
-import { Issuer, TokenSet } from 'openid-client';
+import {
+    ICogniteAuthWrapper,
+    ISettings,
+    LoginMethods,
+} from 'interfaces/common';
+import * as open from 'open';
+import { Client, Issuer, TokenSet, generators } from 'openid-client';
 
 class CogniteAuthWrapper implements ICogniteAuthWrapper {
-    settings: ISettings;
-    checkAuthentication?: boolean;
-    issuer: Issuer;
+    protected settings: ISettings;
+    protected issuer: Issuer;
+    protected client: Client;
 
-    constructor(settings: ISettings, checkAuthentication?: boolean) {
+    constructor(settings: ISettings) {
         this.settings = settings;
-        this.checkAuthentication = checkAuthentication;
     }
 
     // eslint-disable-next-line consistent-return
-    async login(method: LoginMethod): Promise<TokenSet | null> {
-        if (!Object.values(LoginMethod).includes(method)) return null;
+    public async login(
+        method: LoginMethods
+    ): Promise<TokenSet | null | string> {
+        if (!Object.values(LoginMethods).includes(method)) return null;
 
-        if (method === LoginMethod.pkce) {
-            const issuer = await Issuer.discover(this.settings.authority);
-            const client = new issuer.Client({
-                client_id: this.settings.client_id,
-                ...(this.settings.client_secret
-                    ? { client_secret: this.settings.client_secret }
-                    : {}),
-                redirect_uris: [this.settings.redirect_uri],
-                response_types: [this.settings.response_type],
-                post_logout_redirect_uris: [
-                    this.settings.post_logout_redirect_uri,
-                ],
-            });
+        this.issuer = await Issuer.discover(this.settings.authority);
+        this.client = new this.issuer.Client({
+            client_id: this.settings.client_id,
+            ...(this.settings.client_secret
+                ? { client_secret: this.settings.client_secret }
+                : {}),
+            redirect_uris: [this.settings.redirect_uri],
+            response_types: [this.settings.response_type],
+            post_logout_redirect_uris: [this.settings.post_logout_redirect_uri],
+        });
 
-            return client.grant({
-                grant_type: 'client_credentials',
-                scope: this.settings.scope,
-            });
-        }
+        if (method === LoginMethods.CLIENT_CREDENTIALS)
+            return this.handleClientCredentials();
+
+        if (method === LoginMethods.PKCE) return this.handlePkce();
+    }
+
+    private handleClientCredentials(): Promise<TokenSet> {
+        return this.client.grant({
+            grant_type: 'client_credentials',
+        });
+    }
+
+    private async handlePkce(): Promise<string> {
+        const codeVerifier = generators.codeVerifier();
+        const codeChallenge = generators.codeChallenge(codeVerifier);
+
+        const authCodeUrl = this.client.authorizationUrl({
+            code_challenge: codeChallenge,
+            code_challenge_method: 'S256',
+        });
+
+        await open(authCodeUrl);
+
+        return authCodeUrl;
     }
 }
 
