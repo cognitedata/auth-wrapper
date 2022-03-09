e92ee56e792b10bbe52da09cf22ff91ab9c79827
Joao Harbs <harbspj@gmail.com>
2022-03-09 13:15:59 +0100

    feat: added client_credentials little sample

---
+++ samples/client_credentials/quickstart.ts
@@ -0,0 +1,22 @@
+import { LoginMethod } from 'interfaces/common';
+
+import CogniteAuthWrapper from '../../src/index';
+
+const settings = {
+    authority: process.env.COGNITE_AUTHORITY,
+    client_id: process.env.COGNITE_CLIENT_ID,
+    client_secret: process.env.COGNITE_CLIENT_SECRET,
+    redirect_uri: process.env.COGNITE_REDIRECT_URI,
+    post_logout_redirect_uri: process.env.COGNITE_POST_LOGOUT_REDIRECT_URI,
+    response_type: process.env.COGNITE_RESPONSE_TYPE,
+    scope: process.env.COGNITE_SCOPE,
+};
+
+const authWrapper = new CogniteAuthWrapper(settings);
+
+authWrapper
+    .login(LoginMethod.pkce)
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((err) => console.log(err));
