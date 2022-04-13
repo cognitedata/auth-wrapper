6d959b9834bc7d14c7c760c5a51eb94ec2e67d05
Joao Harbs <harbspj@gmail.com>
2022-04-13 15:47:40 +0100

    fix: fixing pkce sample

--- samples/pkce.ts
+++ samples/pkce.ts
@@ -5,7 +5,6 @@ class PkceSample {
     protected settings: ISettings = {
         authority: process.env.COGNITE_AUTHORITY,
         client_id: process.env.COGNITE_CLIENT_ID,
-        client_secret: process.env.COGNITE_CLIENT_SECRET,
         scope: process.env.COGNITE_SCOPE,
     };
 
@@ -13,11 +12,10 @@ class PkceSample {
         if (
             !this.settings.authority ||
             !this.settings.client_id ||
-            !this.settings.client_secret ||
             !this.settings.scope
         )
             throw Error(
-                'You must set the environment variable COGNITE_AUTHORITY, COGNITE_CLIENT_ID COGNITE_CLIENT_SECRET and COGNITE_SCOPE'
+                'You must set the environment variable COGNITE_AUTHORITY, COGNITE_CLIENT_ID and COGNITE_SCOPE'
             );
 
         // Retrieving access_token.
